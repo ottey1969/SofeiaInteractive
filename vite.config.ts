343f22dbe539eea7c27d0f127e6014352dfa92dc
ottey1969 <otteyfrancisca@gmail.com>
2025-07-12 07:00:52 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -31,7 +31,7 @@ export default defineConfig(async ({ mode }) => {
     },
     root: path.resolve(__dirname, "client"),
     build: {
-      outDir: path.resolve(__dirname, "../dist/public")
+      outDir: path.resolve(__dirname, "../dist/public"), // Added comma here
       emptyOutDir: true,
     },
     server: {
