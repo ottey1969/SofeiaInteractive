124a1358464e765b26cd75b9ac752d28a3acdcd9
ottey1969 <otteyfrancisca@gmail.com>
2025-07-13 07:23:11 +0800

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// Call registerRoutes with the 'app' instance
+// Call registerRoutes with the \'app\' instance
 registerRoutes(app);
 
 app.use((err: any, _req: Request, res: Response, next: NextFunction) => {
